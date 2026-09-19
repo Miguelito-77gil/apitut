d03d9157ed0c57dae539dd74b15bde4447787448
Miguel Carillo <carillomiguel77@gmail.com>
2026-09-19 15:36:46 +0800

    Initial project setup

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+﻿import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import hpp from 'hpp';
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(hpp());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+export default app;
